fc6e2c367cb85723e90bafeed012a8ba2e228c30
Jack Collins <jackmpcollins@gmail.com>
2023-07-09 18:32:18 -0700

    Add prompt_chain decorator

---
+++ agentic/prompt_chain.py
@@ -0,0 +1,37 @@
+import inspect
+from functools import update_wrapper
+from typing import Any, Callable, ParamSpec, TypeVar
+
+from agentic.chat import Chat
+from agentic.chat_model.base import FunctionCallMessage
+from agentic.prompt_function import PromptFunction
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+def prompt_chain(
+    template: str,
+    functions: list[Callable[..., Any]] | None = None,
+) -> Callable[[Callable[P, R]], Callable[P, R]]:
+    """Converts a Python function to an LLM query, auto-resolving function calls."""
+
+    def decorator(func: Callable[P, R]) -> Callable[P, R]:
+        func_signature = inspect.signature(func)
+        prompt_function = PromptFunction[P, R](
+            template=template,
+            parameters=list(func_signature.parameters.values()),
+            return_type=func_signature.return_annotation,
+            functions=functions,
+        )
+
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+            chat = Chat.from_prompt(prompt_function, *args, **kwargs).submit()
+            while isinstance(chat.messages[-1], FunctionCallMessage):
+                function_result_message = chat.messages[-1].get_result()
+                chat = chat.add_message(function_result_message).submit()
+            return chat.messages[-1].content
+
+        return update_wrapper(wrapper, func)
+
+    return decorator
---
+++ tests/test_prompt_chain.py
@@ -0,0 +1,22 @@
+from agentic.prompt_chain import prompt_chain
+
+
+def test_prompt_chain():
+    def get_current_weather(location, unit="fahrenheit"):
+        """Get the current weather in a given location"""
+        return {
+            "location": location,
+            "temperature": "72",
+            "unit": unit,
+            "forecast": ["sunny", "windy"],
+        }
+
+    @prompt_chain(
+        template="What's the weather like in {city}?",
+        functions=[get_current_weather],
+    )
+    def describe_weather(city: str) -> str:
+        ...
+
+    output = describe_weather("Boston")
+    assert isinstance(output, str)
