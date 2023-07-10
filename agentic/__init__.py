0b2cd089a3261ad2083570bd6473ef58953b12e1
Jack Collins <jackmpcollins@gmail.com>
2023-07-09 22:04:40 -0700

    Add FunctionCall, prompt_chain, prompt to top-level import path

---
+++ agentic/__init__.py
@@ -0,0 +1,5 @@
+from agentic.function_call import FunctionCall
+from agentic.prompt_chain import prompt_chain
+from agentic.prompt_function import prompt
+
+__all__ = ["FunctionCall", "prompt", "prompt_chain"]
