2d676c985bed0f91418d316b9393f77a694f9aba
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2023-07-22 02:05:53 -0700

    Use __all__ to explicitly export from top-level

--- src/magentic/__init__.py
+++ src/magentic/__init__.py
@@ -1,3 +1,9 @@
 from magentic.function_call import FunctionCall
 from magentic.prompt_chain import prompt_chain
 from magentic.prompt_function import prompt
+
+__all__ = [
+    "FunctionCall",
+    "prompt_chain",
+    "prompt",
+]
