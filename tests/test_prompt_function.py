1e95ccaf508a6c3f5805156d0efe386e842320e1
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2023-07-30 22:37:48 -0700

    Add test for async with function call

--- tests/test_prompt_function.py
+++ tests/test_prompt_function.py
@@ -106,8 +106,6 @@ def test_decorator_return_function_call():
     assert isinstance(output, FunctionCall)
     func_result = output()
     assert isinstance(func_result, int)
-    assert isinstance(func_result, int)
-    assert isinstance(func_result, int)
 
 
 @pytest.mark.asyncio
@@ -120,3 +118,20 @@ async def test_async_decorator_return_str():
 
     assert isinstance(get_capital, AsyncPromptFunction)
     assert await get_capital("Ireland") == "Dublin"
+
+
+@pytest.mark.asyncio
+@pytest.mark.openai
+async def test_async_decorator_return_function_call():
+    def plus(a: int, b: int) -> int:
+        return a + b
+
+    @prompt(functions=[plus])
+    async def sum_populations(country_one: str, country_two: str) -> FunctionCall[int]:
+        """Sum the populations of {country_one} and {country_two}."""
+        ...
+
+    output = await sum_populations("Ireland", "UK")
+    assert isinstance(output, FunctionCall)
+    func_result = output()
+    assert isinstance(func_result, int)
