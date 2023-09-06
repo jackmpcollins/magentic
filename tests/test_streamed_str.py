08ad9c0a12e9b3112dd85019ce105acef43359a7
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2023-09-06 00:24:39 -0700

    Tidy StreamedStr type hints. Improve README example. (#7)

--- tests/test_streamed_str.py
+++ tests/test_streamed_str.py
@@ -1,43 +1,41 @@
-from typing import AsyncIterator, Iterator
+from typing import AsyncIterator
 
 import pytest
 
 from magentic import AsyncStreamedStr, StreamedStr
+from magentic.streamed_str import async_iter
+
+
+@pytest.mark.asyncio
+async def test_async_iter():
+    output = async_iter(["Hello", " World"])
+    assert isinstance(output, AsyncIterator)
+    assert [chunk async for chunk in output] == ["Hello", " World"]
 
 
 def test_streamed_str_iter():
-    def generator() -> Iterator[str]:
-        yield from ["Hello", " World"]
-
-    streamed_str = StreamedStr(generator())
+    iter_chunks = iter(["Hello", " World"])
+    streamed_str = StreamedStr(iter_chunks)
     assert list(streamed_str) == ["Hello", " World"]
+    assert list(iter_chunks) == []  # iterator is exhausted
     assert list(streamed_str) == ["Hello", " World"]
 
 
 def test_streamed_str_str():
-    def generator() -> Iterator[str]:
-        yield from ["Hello", " World"]
-
-    streamed_str = StreamedStr(generator())
+    streamed_str = StreamedStr(["Hello", " World"])
     assert str(streamed_str) == "Hello World"
 
 
 @pytest.mark.asyncio
 async def test_async_streamed_str_iter():
-    async def generator() -> AsyncIterator[str]:
-        for chunk in ["Hello", " World"]:
-            yield chunk
-
-    async_streamed_str = AsyncStreamedStr(generator())
+    aiter_chunks = async_iter(["Hello", " World"])
+    async_streamed_str = AsyncStreamedStr(aiter_chunks)
     assert [chunk async for chunk in async_streamed_str] == ["Hello", " World"]
+    assert [chunk async for chunk in aiter_chunks] == []  # iterator is exhausted
     assert [chunk async for chunk in async_streamed_str] == ["Hello", " World"]
 
 
 @pytest.mark.asyncio
 async def test_async_streamed_str_to_string():
-    async def generator() -> AsyncIterator[str]:
-        for chunk in ["Hello", " World"]:
-            yield chunk
-
-    async_streamed_str = AsyncStreamedStr(generator())
+    async_streamed_str = AsyncStreamedStr(async_iter(["Hello", " World"]))
     assert await async_streamed_str.to_string() == "Hello World"
