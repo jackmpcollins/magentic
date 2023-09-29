229346d4e7d6bed52ad477d0cc2ea31a81d13227
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2023-09-29 00:32:48 -0700

    Add chatprompt and Message classes as top-level imports

--- src/magentic/__init__.py
+++ src/magentic/__init__.py
@@ -1,9 +1,19 @@
+from magentic.chat_model.message import (
+    AssistantMessage,
+    SystemMessage,
+    UserMessage,
+)
+from magentic.chatprompt import chatprompt
 from magentic.function_call import FunctionCall
 from magentic.prompt_chain import prompt_chain
 from magentic.prompt_function import prompt
 from magentic.streaming import AsyncStreamedStr, StreamedStr
 
 __all__ = [
+    "AssistantMessage",
+    "SystemMessage",
+    "UserMessage",
+    "chatprompt",
     "FunctionCall",
     "prompt_chain",
     "prompt",
