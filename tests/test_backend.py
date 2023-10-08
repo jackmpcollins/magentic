a8f3e28a28ad919652195355dc029a669d907023
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2023-10-07 18:07:45 -0700

    Update tests for get_chat_model

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -1,13 +1,12 @@
-from magentic.chat_model.openai_chat_model import (
-    OpenaiChatModel,
-)
+from magentic.backend import get_chat_model
 
 
 def test_openai_chat_model_model(monkeypatch):
+    monkeypatch.setenv("MAGENTIC_BACKEND", "openai")
     monkeypatch.setenv("MAGENTIC_OPENAI_MODEL", "gpt-4")
-    assert OpenaiChatModel().model == "gpt-4"
+    assert get_chat_model().model == "gpt-4"
 
 
 def test_openai_chat_model_temperature(monkeypatch):
     monkeypatch.setenv("MAGENTIC_OPENAI_TEMPERATURE", "2")
-    assert OpenaiChatModel().temperature == 2
+    assert get_chat_model().temperature == 2
