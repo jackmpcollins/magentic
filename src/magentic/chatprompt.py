0319c675e19cb832b8e13bb1f8b46e5c38fde10c
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2023-10-30 22:55:10 -0700

    Replace black with ruff formatter (#48)

--- src/magentic/chatprompt.py
+++ src/magentic/chatprompt.py
@@ -123,7 +123,9 @@ class ChatPromptDecorator(Protocol):
     """
 
     @overload
-    def __call__(self, func: Callable[P, Awaitable[R]]) -> AsyncChatPromptFunction[P, R]:  # type: ignore[misc]
+    def __call__(  # type: ignore[misc]
+        self, func: Callable[P, Awaitable[R]]
+    ) -> AsyncChatPromptFunction[P, R]:
         ...
 
     @overload
