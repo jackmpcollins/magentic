c1ba1187a07d22816ed84c91ae383a4f75dafe8e
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2023-11-01 22:00:23 -0700

    Add tests for chat model context

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -30,3 +30,19 @@ def test_openai_chat_model_completion():
     assert isinstance(response, AssistantMessage)
     assert isinstance(response.content, str)
     # TODO: test for num_tokens here
+
+
+def test_chat_model_context():
+    chat_model = OpenaiChatModel("gpt-4")
+    with chat_model:
+        assert get_chat_model() is chat_model
+
+
+def test_chat_model_context_within_context():
+    with OpenaiChatModel("gpt-4"):
+        assert get_chat_model().model == "gpt-4"  # type: ignore[attr-defined]
+
+        with OpenaiChatModel("gpt-5"):
+            assert get_chat_model().model == "gpt-5"  # type: ignore[attr-defined]
+
+        assert get_chat_model().model == "gpt-4"  # type: ignore[attr-defined]
