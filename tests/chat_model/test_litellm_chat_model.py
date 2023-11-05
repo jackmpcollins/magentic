397f569860873eeaa65da889fdad96e4509611c6
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2023-11-04 21:54:48 -0700

    Add tests for LitellmChatModel

---
+++ tests/chat_model/test_litellm_chat_model.py
@@ -0,0 +1,34 @@
+import pytest
+
+from magentic.chat_model.litellm_chat_model import LitellmChatModel
+from magentic.chat_model.message import UserMessage
+
+
+@pytest.mark.openai
+def test_litellm_chat_model_complete_openai():
+    chat_model = LitellmChatModel("gpt-3.5-turbo")
+    message = chat_model.complete(messages=[UserMessage("Say hello!")])
+    assert isinstance(message.content, str)
+
+
+@pytest.mark.anthropic
+def test_litellm_chat_model_complete_anthropic():
+    chat_model = LitellmChatModel("claude-2")
+    message = chat_model.complete(messages=[UserMessage("Say hello!")])
+    assert isinstance(message.content, str)
+
+
+@pytest.mark.asyncio
+@pytest.mark.openai
+async def test_litellm_chat_model_acomplete_openai():
+    chat_model = LitellmChatModel("gpt-3.5-turbo")
+    message = await chat_model.acomplete(messages=[UserMessage("Say hello!")])
+    assert isinstance(message.content, str)
+
+
+@pytest.mark.asyncio
+@pytest.mark.anthropic
+async def test_litellm_chat_model_acomplete_anthropic():
+    chat_model = LitellmChatModel("claude-2")
+    message = await chat_model.acomplete(messages=[UserMessage("Say hello!")])
+    assert isinstance(message.content, str)
