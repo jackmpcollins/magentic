07fb16647ac9229e843d01470ee5678a017af520
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2023-11-06 21:17:05 -0800

    TEMP: Comment LiteLLM tests

--- tests/chat_model/test_litellm_chat_model.py
+++ tests/chat_model/test_litellm_chat_model.py
@@ -1,64 +1,64 @@
-import litellm
-import pytest
+# import litellm
+# import pytest
 
-from magentic.chat_model.litellm_chat_model import LitellmChatModel
-from magentic.chat_model.message import UserMessage
-
-
-@pytest.mark.openai
-def test_litellm_chat_model_complete_openai():
-    chat_model = LitellmChatModel("gpt-3.5-turbo")
-    message = chat_model.complete(messages=[UserMessage("Say hello!")])
-    assert isinstance(message.content, str)
+# from magentic.chat_model.litellm_chat_model import LitellmChatModel
+# from magentic.chat_model.message import UserMessage
 
 
-@pytest.mark.anthropic
-def test_litellm_chat_model_complete_anthropic():
-    chat_model = LitellmChatModel("claude-2")
-    message = chat_model.complete(messages=[UserMessage("Say hello!")])
-    assert isinstance(message.content, str)
+# @pytest.mark.openai
+# def test_litellm_chat_model_complete_openai():
+#     chat_model = LitellmChatModel("gpt-3.5-turbo")
+#     message = chat_model.complete(messages=[UserMessage("Say hello!")])
+#     assert isinstance(message.content, str)
 
 
-@pytest.mark.ollama
-def test_litellm_chat_model_complete_ollama():
-    chat_model = LitellmChatModel("ollama/llama2", api_base="http://localhost:11434")
-    message = chat_model.complete(messages=[UserMessage("Say hello!")])
-    assert isinstance(message.content, str)
+# @pytest.mark.anthropic
+# def test_litellm_chat_model_complete_anthropic():
+#     chat_model = LitellmChatModel("claude-2")
+#     message = chat_model.complete(messages=[UserMessage("Say hello!")])
+#     assert isinstance(message.content, str)
 
 
-@pytest.mark.anthropic
-def test_litellm_chat_model_complete_anthropic_function_calling_error():
-    def sum(a: int, b: int) -> int:
-        """Sum two numbers."""
-        return a + b
-
-    chat_model = LitellmChatModel("claude-2")
-    with pytest.raises(litellm.exceptions.ServiceUnavailableError):
-        chat_model.complete(messages=[UserMessage("Say hello!")], functions=[sum])
+# @pytest.mark.ollama
+# def test_litellm_chat_model_complete_ollama():
+#     chat_model = LitellmChatModel("ollama/llama2", api_base="http://localhost:11434")
+#     message = chat_model.complete(messages=[UserMessage("Say hello!")])
+#     assert isinstance(message.content, str)
 
 
-@pytest.mark.ollama
-def test_litellm_chat_model_complete_ollama_function_calling_error():
-    def sum(a: int, b: int) -> int:
-        """Sum two numbers."""
-        return a + b
+# @pytest.mark.anthropic
+# def test_litellm_chat_model_complete_anthropic_function_calling_error():
+#     def sum(a: int, b: int) -> int:
+#         """Sum two numbers."""
+#         return a + b
 
-    chat_model = LitellmChatModel("ollama/llama2", api_base="http://localhost:11434")
-    with pytest.raises(litellm.exceptions.APIError):
-        chat_model.complete(messages=[UserMessage("Say hello!")], functions=[sum])
-
-
-@pytest.mark.asyncio
-@pytest.mark.openai
-async def test_litellm_chat_model_acomplete_openai():
-    chat_model = LitellmChatModel("gpt-3.5-turbo")
-    message = await chat_model.acomplete(messages=[UserMessage("Say hello!")])
-    assert isinstance(message.content, str)
+#     chat_model = LitellmChatModel("claude-2")
+#     with pytest.raises(litellm.exceptions.ServiceUnavailableError):
+#         chat_model.complete(messages=[UserMessage("Say hello!")], functions=[sum])
 
 
-@pytest.mark.asyncio
-@pytest.mark.anthropic
-async def test_litellm_chat_model_acomplete_anthropic():
-    chat_model = LitellmChatModel("claude-2")
-    message = await chat_model.acomplete(messages=[UserMessage("Say hello!")])
-    assert isinstance(message.content, str)
+# @pytest.mark.ollama
+# def test_litellm_chat_model_complete_ollama_function_calling_error():
+#     def sum(a: int, b: int) -> int:
+#         """Sum two numbers."""
+#         return a + b
+
+#     chat_model = LitellmChatModel("ollama/llama2", api_base="http://localhost:11434")
+#     with pytest.raises(litellm.exceptions.APIError):
+#         chat_model.complete(messages=[UserMessage("Say hello!")], functions=[sum])
+
+
+# @pytest.mark.asyncio
+# @pytest.mark.openai
+# async def test_litellm_chat_model_acomplete_openai():
+#     chat_model = LitellmChatModel("gpt-3.5-turbo")
+#     message = await chat_model.acomplete(messages=[UserMessage("Say hello!")])
+#     assert isinstance(message.content, str)
+
+
+# @pytest.mark.asyncio
+# @pytest.mark.anthropic
+# async def test_litellm_chat_model_acomplete_anthropic():
+#     chat_model = LitellmChatModel("claude-2")
+#     message = await chat_model.acomplete(messages=[UserMessage("Say hello!")])
+#     assert isinstance(message.content, str)
