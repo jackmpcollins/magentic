996d6b84a6adea42f725b1aa9d1d4133f6fd6dad
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2023-11-28 21:43:11 -0800

    Add test for OpenaiChatModel seed

--- tests/chat_model/test_openai_chat_model.py
+++ tests/chat_model/test_openai_chat_model.py
@@ -9,3 +9,11 @@ def test_openai_chat_model_complete_base_url():
     chat_model = OpenaiChatModel("gpt-3.5-turbo", base_url="https://api.openai.com/v1")
     message = chat_model.complete(messages=[UserMessage("Say hello!")])
     assert isinstance(message.content, str)
+
+
+@pytest.mark.openai
+def test_openai_chat_model_complete_seed():
+    chat_model = OpenaiChatModel("gpt-3.5-turbo", seed=42)
+    message1 = chat_model.complete(messages=[UserMessage("Say hello!")])
+    message2 = chat_model.complete(messages=[UserMessage("Say hello!")])
+    assert message1.content == message2.content
