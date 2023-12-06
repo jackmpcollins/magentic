995cc1b936617181280169c26c0b02dc23a22c9d
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2023-12-06 01:08:39 -0800

    Add api_key param to OpenaiChatModel

--- tests/chat_model/test_openai_chat_model.py
+++ tests/chat_model/test_openai_chat_model.py
@@ -1,7 +1,24 @@
+import os
+
+import openai
 import pytest
 
 from magentic.chat_model.message import UserMessage
 from magentic.chat_model.openai_chat_model import OpenaiChatModel
+
+
+@pytest.mark.openai
+def test_openai_chat_model_api_key(monkeypatch):
+    openai_api_key = os.environ["OPENAI_API_KEY"]
+    monkeypatch.delenv("OPENAI_API_KEY")
+
+    chat_model = OpenaiChatModel("gpt-3.5-turbo")
+    with pytest.raises(openai.OpenAIError):
+        chat_model.complete(messages=[UserMessage("Say hello!")])
+
+    chat_model = OpenaiChatModel("gpt-3.5-turbo", api_key=openai_api_key)
+    message = chat_model.complete(messages=[UserMessage("Say hello!")])
+    assert isinstance(message.content, str)
 
 
 @pytest.mark.openai
