191aab2665a9e743521f369da3157c504927b542
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2024-02-19 17:54:42 -0800

    Add ImageUserMessage that formats using kwargs

--- src/magentic/vision.py
+++ src/magentic/vision.py
@@ -1,4 +1,5 @@
-from typing import Any, Generic, Literal, Sequence, TypeVar, cast
+import base64
+from typing import Any, Generic, Literal, Sequence, TypeVar, cast, overload
 
 from openai.types.chat import ChatCompletionContentPartParam, ChatCompletionMessageParam
 from pydantic import BaseModel
@@ -15,7 +16,7 @@ class ImageUrl(BaseModel):
     detail: Literal["auto", "low", "high"] = "auto"
 
 
-ContentT = TypeVar("ContentT")
+ContentT = TypeVar("ContentT", bound=Sequence[str | ImageUrl])
 
 
 class MultipartUserMessage(Message[Sequence[ContentT]], Generic[ContentT]):
@@ -52,3 +53,64 @@ def _(message: MultipartUserMessage[Any]) -> ChatCompletionMessageParam:
             raise TypeError(msg)
 
     return {"role": OpenaiMessageRole.USER.value, "content": content}
+
+
+ImageContentT = TypeVar("ImageContentT", bound=bytes | str)
+
+
+class ImageUserMessage(Message[ImageContentT]):
+    @overload
+    def format(
+        self: "ImageUserMessage[bytes]", **kwargs: Any
+    ) -> "ImageUserMessage[bytes]":
+        ...
+
+    @overload
+    def format(
+        self: "ImageUserMessage[str]", **kwargs: Any
+    ) -> "ImageUserMessage[bytes] | ImageUserMessage[str]":
+        ...
+
+    def format(
+        self: "ImageUserMessage[bytes] | ImageUserMessage[str]", **kwargs: Any
+    ) -> "ImageUserMessage[bytes] | ImageUserMessage[str]":
+        """Format the message using the given function arguments.
+
+        - If existing content is `bytes`, it is returned as is.
+        - If existing content is a string
+            - Check if the string is a key in `kwargs` and use the value from `kwargs` if it is.
+            - Call `str.format` with the given `kwargs` and return the result.
+        """
+        if isinstance(self.content, bytes):
+            return ImageUserMessage(self.content)
+        if isinstance(self.content, str):
+            if self.content in kwargs:
+                if not isinstance(kwargs[self.content], bytes | str):
+                    msg = f"Unsupported content type: {type(kwargs[self.content])}"
+                    raise TypeError(msg)
+                return ImageUserMessage(kwargs[self.content])
+            return ImageUserMessage(self.content.format(**kwargs))
+        msg = f"Unsupported content type: {type(self.content)}"
+        raise TypeError(msg)
+
+
+@message_to_openai_message.register(ImageUserMessage)
+def _(message: ImageUserMessage[bytes | str]) -> ChatCompletionMessageParam:
+    if isinstance(message.content, str):
+        url = message.content
+    else:
+        base64_image = base64.b64encode(message.content).decode("utf-8")
+        url = f"data:image/jpeg;base64,{base64_image}"
+
+    return {
+        "role": OpenaiMessageRole.USER.value,
+        "content": [
+            {
+                "type": "image_url",
+                "image_url": {
+                    "url": url,
+                    "detail": "auto",
+                },
+            }
+        ],
+    }
