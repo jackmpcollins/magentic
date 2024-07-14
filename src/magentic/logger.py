57c3f8241b3e2fa75000ab3331bfe23333dfd19f
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2024-07-13 22:16:42 -0700

    Create set_verbose to set from python

--- src/magentic/logger.py
+++ src/magentic/logger.py
@@ -1,14 +1,24 @@
 import logging
 import sys
+from functools import cache
 
 from magentic.settings import get_settings
 
-settings = get_settings()
-
 logger = logging.getLogger("magentic")
 
-if settings.verbose:
-    logger.addHandler(logging.StreamHandler(sys.stdout))
-
+# Set default log level to WARNING so magentic INFO logs must be explicitly enabled
 if logger.level == logging.NOTSET:
-    logger.setLevel(logging.INFO if settings.verbose else logging.WARNING)
+    logger.setLevel(logging.WARNING)
+
+
+@cache  # Can only be called once
+def set_verbose():
+    """Set the magentic logger to print INFO level messages to stdout."""
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    if logger.level > logging.INFO:
+        logger.setLevel(logging.INFO)
+
+
+settings = get_settings()
+if settings.verbose:
+    set_verbose()
