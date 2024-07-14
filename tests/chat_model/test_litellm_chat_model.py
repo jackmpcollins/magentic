52a6324d36e8fdbabe98d241a18ca794c7a08a61
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2024-07-14 00:52:41 -0700

    Fix breaking test for litellm custom_llm_provider

--- tests/chat_model/test_litellm_chat_model.py
+++ tests/chat_model/test_litellm_chat_model.py
@@ -60,11 +60,11 @@ def test_litellm_chat_model_metadata(litellm_success_callback_calls):
 @pytest.mark.litellm_openai
 def test_litellm_chat_model_custom_llm_provider(litellm_success_callback_calls):
     """Test that provided custom_llm_provider is passed to the litellm success callback."""
-    chat_model = LitellmChatModel("gpt-3.5-turbo", custom_llm_provider="custom")
-    assert chat_model.custom_llm_provider == "custom"
+    chat_model = LitellmChatModel("gpt-3.5-turbo", custom_llm_provider="openai")
+    assert chat_model.custom_llm_provider == "openai"
     chat_model.complete(messages=[UserMessage("Say hello!")])
     callback_call = litellm_success_callback_calls[-1]
-    assert callback_call["kwargs"]["litellm_params"]["custom_llm_provider"] == "custom"
+    assert callback_call["kwargs"]["litellm_params"]["custom_llm_provider"] == "openai"
 
 
 @pytest.fixture()
@@ -103,11 +103,11 @@ async def test_litellm_chat_model_custom_llm_provider_async(
     litellm_async_success_callback_calls,
 ):
     """Test that provided custom_llm_provider is passed to the litellm success callback."""
-    chat_model = LitellmChatModel("gpt-3.5-turbo", custom_llm_provider="custom")
-    assert chat_model.custom_llm_provider == "custom"
+    chat_model = LitellmChatModel("gpt-3.5-turbo", custom_llm_provider="openai")
+    assert chat_model.custom_llm_provider == "openai"
     await chat_model.acomplete(messages=[UserMessage("Say hello!")])
     callback_call = litellm_async_success_callback_calls[-1]
-    assert callback_call["kwargs"]["litellm_params"]["custom_llm_provider"] == "custom"
+    assert callback_call["kwargs"]["litellm_params"]["custom_llm_provider"] == "openai"
 
 
 @pytest.mark.parametrize(
