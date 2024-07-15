11caf7f4649abdcc1135eb87feca36eb82c358a2
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2024-07-14 20:38:24 -0700

    Add TODOs for setting scope

--- src/magentic/logger.py
+++ src/magentic/logger.py
@@ -7,4 +7,7 @@ logger = logging.getLogger("magentic")
 if logger.level == logging.NOTSET:
     logger.setLevel(logging.WARNING)
 
-logfire = logfire_api.with_settings(custom_scope_suffix="magentic")
+logfire = logfire_api.with_settings(
+    custom_scope_suffix="magentic",  # TODO: This should be scope, not suffix
+    otel_scope_version="",  # TODO: Not currently accepted
+)
