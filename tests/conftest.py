ba1c18d1f8a0daa22f3f79b74015b18b3d69a8e7
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2024-11-16 00:58:22 -0800

    Configure vcr for openai requests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,16 @@ from dotenv import load_dotenv
 @pytest.fixture(autouse=True, scope="session")
 def _load_dotenv():
     load_dotenv()
+
+
+@pytest.fixture(autouse=True, scope="session")
+def vcr_config():
+    def before_record_response(response):
+        del response["headers"]["openai-organization"]
+        del response["headers"]["Set-Cookie"]
+        return response
+
+    return {
+        "filter_headers": ["authorization", "openai-organization"],
+        "before_record_response": before_record_response,
+    }
