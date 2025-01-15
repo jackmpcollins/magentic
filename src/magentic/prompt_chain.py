596d77d33b2ba3f6e6afb24f13e4e7197d8eada6
Jack Collins <6640905+jackmpcollins@users.noreply.github.com>
2025-01-14 22:54:17 -0800

    Allow Messages as input to prompt_chain (#403)
    * Allow Messages as input to prompt_chain
    
    * Add tests for prompt_chain with messages
    
    * Mention prompt_chain accepting Messages in docs
    
    * Update docs and prompt_chain docstring

--- src/magentic/prompt_chain.py
+++ src/magentic/prompt_chain.py
@@ -1,14 +1,14 @@
 import inspect
-from collections.abc import Callable
+from collections.abc import Callable, Sequence
 from functools import wraps
 from typing import Any, ParamSpec, TypeVar, cast
 
 from magentic._chat import Chat
 from magentic.chat_model.base import ChatModel
-from magentic.chat_model.message import UserMessage
+from magentic.chat_model.message import Message, UserMessage
+from magentic.chatprompt import AsyncChatPromptFunction, ChatPromptFunction
 from magentic.function_call import FunctionCall
 from magentic.logger import logfire
-from magentic.prompt_function import AsyncPromptFunction, PromptFunction
 
 P = ParamSpec("P")
 R = TypeVar("R")
@@ -19,23 +19,37 @@ class MaxFunctionCallsError(Exception):
 
 
 def prompt_chain(
-    template: str,
+    template: str | Sequence[Message[Any]],
     functions: list[Callable[..., Any]] | None = None,
     model: ChatModel | None = None,
     max_calls: int | None = None,
 ) -> Callable[[Callable[P, R]], Callable[P, R]]:
-    """Convert a Python function to an LLM query, auto-resolving function calls."""
+    """Convert a Python function to an LLM query, auto-resolving function calls.
+
+    Use `@prompt_chain` when you need the LLM to perform multiple function calls to
+    reach a final answer. When a function decorated with `@prompt_chain` is called, the
+    LLM is queried, then any function calls are automatically executed and the results
+    appended to the list of messages. Then the LLM is queried again and this repeats
+    until a final answer is reached.
+
+    Set `max_calls` to limit the number of function calls. If the limit is reached, a
+    `MaxFunctionCallsError` will be raised.
+    """
+
+    messages = (
+        [UserMessage(content=template)] if isinstance(template, str) else template
+    )
 
     def decorator(func: Callable[P, R]) -> Callable[P, R]:
         func_signature = inspect.signature(func)
 
         if inspect.iscoroutinefunction(func):
-            async_prompt_function = AsyncPromptFunction[P, Any](
+            async_prompt_function = AsyncChatPromptFunction[P, Any](
                 name=func.__name__,
                 parameters=list(func_signature.parameters.values()),
                 # TODO: Also allow ParallelFunctionCall. Support this more neatly
                 return_type=func_signature.return_annotation | FunctionCall,  # type: ignore[arg-type,unused-ignore]
-                template=template,
+                messages=messages,
                 functions=functions,
                 model=model,
             )
@@ -47,11 +61,7 @@ def prompt_chain(
                     **func_signature.bind(*args, **kwargs).arguments,
                 ):
                     chat = await Chat(
-                        messages=[
-                            UserMessage(
-                                content=async_prompt_function.format(*args, **kwargs)
-                            )
-                        ],
+                        messages=async_prompt_function.format(*args, **kwargs),
                         functions=async_prompt_function.functions,
                         output_types=async_prompt_function.return_types,
                         model=async_prompt_function._model,  # Keep `None` value if unset
@@ -71,12 +81,12 @@ def prompt_chain(
 
             return cast(Callable[P, R], awrapper)
 
-        prompt_function = PromptFunction[P, R](
+        prompt_function = ChatPromptFunction[P, R](
             name=func.__name__,
             parameters=list(func_signature.parameters.values()),
             # TODO: Also allow ParallelFunctionCall. Support this more neatly
             return_type=func_signature.return_annotation | FunctionCall,  # type: ignore[arg-type,unused-ignore]
-            template=template,
+            messages=messages,
             functions=functions,
             model=model,
         )
@@ -88,9 +98,7 @@ def prompt_chain(
                 **func_signature.bind(*args, **kwargs).arguments,
             ):
                 chat = Chat(
-                    messages=[
-                        UserMessage(content=prompt_function.format(*args, **kwargs))
-                    ],
+                    messages=prompt_function.format(*args, **kwargs),
                     functions=prompt_function.functions,
                     output_types=prompt_function.return_types,
                     model=prompt_function._model,  # Keep `None` value if unset
