63ffd3ee1868db44a9e897bbc4c8087c0eb1b56d
Strongghj <157555818+Strongghj@users.noreply.github.com>
2025-12-16 14:29:34 +0800

    Clean prompt_chain streaming handling

--- src/magentic/prompt_chain.py
+++ src/magentic/prompt_chain.py
@@ -3,11 +3,14 @@ from collections.abc import Callable, Sequence
 from functools import wraps
 from typing import Any, ParamSpec, TypeVar, cast
 
+from magentic.streaming import StreamedStr
+from magentic import ParallelFunctionCall
+from magentic._streamed_response import StreamedResponse, AsyncStreamedResponse
 from magentic._chat import Chat
 from magentic.chat_model.base import ChatModel
 from magentic.chat_model.message import Message, UserMessage
 from magentic.chatprompt import AsyncChatPromptFunction, ChatPromptFunction
-from magentic.function_call import FunctionCall
+from magentic.function_call import FunctionCall, AsyncParallelFunctionCall
 from magentic.logger import logfire
 
 P = ParamSpec("P")
@@ -18,36 +21,27 @@ class MaxFunctionCallsError(Exception):
     """Raised when prompt chain reaches the max number of function calls."""
 
 
+async def list_to_async_iter(list):
+    for item in list:
+        yield item
+
+
 def prompt_chain(
     template: str | Sequence[Message[Any]],
     functions: list[Callable[..., Any]] | None = None,
     model: ChatModel | None = None,
     max_calls: int | None = None,
 ) -> Callable[[Callable[P, R]], Callable[P, R]]:
-    """Convert a Python function to an LLM query, auto-resolving function calls.
-
-    Use `@prompt_chain` when you need the LLM to perform multiple function calls to
-    reach a final answer. When a function decorated with `@prompt_chain` is called, the
-    LLM is queried, then any function calls are automatically executed and the results
-    appended to the list of messages. Then the LLM is queried again and this repeats
-    until a final answer is reached.
-
-    Set `max_calls` to limit the number of function calls. If the limit is reached, a
-    `MaxFunctionCallsError` will be raised.
-    """
-
     messages = (
         [UserMessage(content=template)] if isinstance(template, str) else template
     )
 
     def decorator(func: Callable[P, R]) -> Callable[P, R]:
         func_signature = inspect.signature(func)
-
         if inspect.iscoroutinefunction(func):
             async_prompt_function = AsyncChatPromptFunction[P, Any](
                 name=func.__name__,
                 parameters=list(func_signature.parameters.values()),
-                # TODO: Also allow ParallelFunctionCall. Support this more neatly
                 return_type=func_signature.return_annotation | FunctionCall,  # type: ignore[arg-type,unused-ignore]
                 messages=messages,
                 functions=functions,
@@ -67,24 +61,37 @@ def prompt_chain(
                         model=async_prompt_function._model,  # Keep `None` value if unset
                     ).asubmit()
                     num_calls = 0
-                    while isinstance(chat.last_message.content, FunctionCall):
+                    while isinstance(chat.last_message.content, AsyncStreamedResponse):
+                        function_calls = []
+                        is_break = True
+                        async for item in chat.last_message.content:
+                            if isinstance(item, FunctionCall):
+                                function_calls.append(item)
+                                is_break = False
                         if max_calls is not None and num_calls >= max_calls:
                             msg = (
                                 f"Function {func.__name__} reached limit of"
                                 f" {max_calls} function calls"
                             )
                             raise MaxFunctionCallsError(msg)
-                        chat = await chat.aexec_function_call()
-                        chat = await chat.asubmit()
-                        num_calls += 1
-                    return chat.last_message.content
+                        if len(function_calls) == 1:
+                            chat = await chat.aexec_function_call(function_calls[0])
+                            chat = await chat.asubmit()
+                            num_calls += 1
+                        elif len(function_calls) > 1:
+                            function_calls = list_to_async_iter(function_calls)
+                            multi_functions = AsyncParallelFunctionCall(function_calls)
+                            chat = await chat.aexec_function_call(multi_functions)
+                            chat = await chat.asubmit()
+                            num_calls += len(function_calls)
+                        if is_break:
+                            break
+                    return chat.last_message.content, chat.messages
 
             return cast(Callable[P, R], awrapper)
-
         prompt_function = ChatPromptFunction[P, R](
             name=func.__name__,
             parameters=list(func_signature.parameters.values()),
-            # TODO: Also allow ParallelFunctionCall. Support this more neatly
             return_type=func_signature.return_annotation | FunctionCall,  # type: ignore[arg-type,unused-ignore]
             messages=messages,
             functions=functions,
@@ -104,16 +111,30 @@ def prompt_chain(
                     model=prompt_function._model,  # Keep `None` value if unset
                 ).submit()
                 num_calls = 0
-                while isinstance(chat.last_message.content, FunctionCall):
+                while isinstance(chat.last_message.content, StreamedResponse):
+                    function_calls = []
+                    is_break = True
+                    for item in chat.last_message.content:
+                        if isinstance(item, FunctionCall):
+                            function_calls.append(item)
+                            is_break = False
                     if max_calls is not None and num_calls >= max_calls:
                         msg = (
                             f"Function {func.__name__} reached limit of"
                             f" {max_calls} function calls"
                         )
                         raise MaxFunctionCallsError(msg)
-                    chat = chat.exec_function_call().submit()
-                    num_calls += 1
-                return cast(R, chat.last_message.content)
+
+                    if len(function_calls) == 1:
+                        chat = chat.exec_function_call(function_calls[0]).submit()
+                        num_calls += 1
+                    elif len(function_calls) > 1:
+                        multi_functions = ParallelFunctionCall(function_calls)
+                        chat = chat.exec_function_call(multi_functions).submit()
+                        num_calls += len(function_calls)
+                    if is_break:
+                        break
+                return chat.last_message.content, chat.messages
 
         return wrapper
 
